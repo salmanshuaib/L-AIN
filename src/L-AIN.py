aca4e54c3080806b78e9ada815c88e7ed8d06d2f
salmanshuaib <me@salmanshuaib.com>
2023-02-26 05:43:01 -0500

    :scroll: getting things started

---
+++ src/L-AIN.py
@@ -0,0 +1,75 @@
+class ASCENDING_FALCONS:    
+    def __init__(self,entry):    
+        self.entry = entry
+       
+
+def Ascending_Falcons(): #Enter input   
+    entry = input("Hi! I am L-AIN..\n")
+    csaid = (entry)
+    return csaid
+
+
+'''uNIVERSAL Nash Equilibrium Strategy is based on Professor John Nash's discovery of the Game Theory principle
+that only that agent is rational who follows a singular decisionmaking model throughout. In our case we use the 
+Trinity: Passion Compromise Budget'''
+def uNESt(enquiry): 
+    if (test_jet == 
+
+'''knowledge maanagement based on Assignment Management System theorem  6<=24 or 3602<=2;
+if you sum the digits of ANY number they resolve to 1,2,3,4,5,5,6,7,8,9; which means 
+9 Cores can direct Infinitizing Tasks - similarly: here nine digits are enough to 
+deal with any amount of data input because the user provides feedback on a scale of 1 to 9_
+'''
+def AMS:    
+
+test_jet = Ascending_Falcons()
+print(uNEST(test_jet))
+#print(test_jet)
+
+'''def sum(sheep): #Sum csaid string
+    Sum = 0
+    while(sheep > 0):
+        Remainder = sheep % 10
+        Sum = Sum + Remainder
+        sheep = sheep //10
+    return Sum
+
+def firm(RESOLVE): #Resolve string to 1~9
+    firm = 0
+    while RESOLVE > 9:
+        Marks = RESOLVE % 10
+        firm = firm + Marks
+        RESOLVE = RESOLVE //10
+    return RESOLVE
+
+def presentID():
+    CD = Battling_Eagles() #Output
+    eagle = len(CD)
+    print ("\nYour CSAID (Cat Standard Alpha ID): ", CD)
+
+    CSAID = sum(eagle)
+    Command = firm(CSAID)
+    print ("You belong to:", Command, "Command")
+
+presentID()
+    
+
+def presentCARD():
+    # Importing required module
+    import subprocess
+
+    # Using system() method to
+    # execute shell commands
+    subprocess.Popen('echo business_card_generator', shell=True)
+
+
+def ack():
+    Continues = input('Generating Cat Card courtesy [ThatGenZGamer48], Press ENTER to continue: ')
+    presentCARD()        
+
+ack()
+'''
+
+
+
+
